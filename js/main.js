ced5aa177731ba35ec31f4578e6d0c69c2d4b4b5
dilshan97 <dilshanramesh81@gmail.com>
2019-08-17 20:52:41 +0530

    color change jquery added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,67 @@
 $(document).ready(function(){
+
     console.log('jquery ready');
 
-    $(function() {
-        $(".dial").knob();
-        $(".red").knob();
+    $(".red").knob({
+        'change' : function (v) { 
+            console.log(v);
+            tb_change();
+        },
+        'release' : function (v) { 
+            console.log(v);
+            color_change();
+         }
     });
+    
+    $(".green").knob({
+        'change' : function (v) { 
+            console.log(v);
+            tb_change();
+        },
+        'release' : function (v) { 
+            console.log(v);
+            color_change();
+         }
+    });
+
+    $(".blue").knob({
+        'change' : function (v) { 
+            console.log(v);
+            tb_change();
+        },
+        'release' : function (v) { 
+            console.log(v);
+            color_change();
+         }
+    });
+
+    function color_change(){
+        var R = $('.red').val();
+        var G = $('.green').val();
+        var B = $('.blue').val();
+
+        var ESP_IP = "192.168.1.5";
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": urlr,
+            "method": "GET",
+            "headers": {
+            "cache-control": "no-cache"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+        
+        console.log(R);
+        console.log(G);
+        console.log(B);
+    }
+
+    function tb_change(){
+
+    }
 });
